220378c1f7b0bfdff003e84b9201802a4e35b880
Mikl Kotyk <mikl.kotyk@gmail.com>
2024-03-29 12:57:41 +0200

    feat: shake sign in button if login false

--- BankeyApp/BankeyApp/Login/LoginViewController.swift
+++ BankeyApp/BankeyApp/Login/LoginViewController.swift
@@ -173,7 +173,7 @@ extension LoginViewController  {
             return
         }
         
-        if username == "a" && password == "1" {
+        if username == "Mikl" && password == "welcome" {
             signInButton.configuration?.showsActivityIndicator = true
             delegate?.didLogin()
         } else {
@@ -184,6 +184,18 @@ extension LoginViewController  {
     private func configureView(withMessage message : String ){
         errorMessageLabel.isHidden = false
         errorMessageLabel.text = message
+        shakeButton()
+    }
+    
+    private func shakeButton() {
+        let animation = CAKeyframeAnimation()
+        animation.keyPath = "position.x"
+        animation.values = [0,10,-10,10,0]
+        animation.keyTimes = [0, 0.16, 0.5, 0.83, 1]
+        animation.duration = 0.4
+        
+        animation.isAdditive = true
+        signInButton.layer.add(animation, forKey: "shake")
     }
 }
 
