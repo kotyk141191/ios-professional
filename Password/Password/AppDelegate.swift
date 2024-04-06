6c793105ed796eedf791b9a95fc5e9e02b72f71d
Pavlo Kabanets <pav.kaban@gmail.com>
2024-04-06 20:03:42 +0300

    merge refresh to main

--- Password/Password/AppDelegate.swift
+++ Password/Password/AppDelegate.swift
@@ -17,7 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         
         window = UIWindow(frame: UIScreen.main.bounds)
         window?.makeKeyAndVisible()
-        window?.backgroundColor = .magenta
+        window?.backgroundColor = .systemTeal
         window?.rootViewController = ViewController()
         
         return true
